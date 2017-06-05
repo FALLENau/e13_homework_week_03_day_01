4686532b9e287eb90547270af045d704d14e5647
FALLENau <reece.c.jones@gmail.com>
2017-06-05 12:08:46 +0100

    When Eugene attarcks

--- jedi.sql
+++ jedi.sql
@@ -12,4 +12,18 @@ INSERT INTO jedi (name, age) VALUES ('Eugene', 100);
 
 INSERT INTO jedi (name, darkside, age) VALUES ('Vader', true, 101);
 
+UPDATE jedi SET darkside = true;
+
+UPDATE jedi SET darkside = false WHERE name = 'Luke';
+
+UPDATE jedi SET age = 27 WHERE name = 'Eugene' AND darkside = true;
+
+INSERT INTO jedi (name, darkside, age) VALUES ('Anakin', false, 12);
+
+UPDATE jedi SET age = 22 WHERE name = 'Anakin';
+
+UPDATE jedi SET darkside = true WHERE name = 'Anakin';
+
+
+
 SELECT * FROM jedi;
