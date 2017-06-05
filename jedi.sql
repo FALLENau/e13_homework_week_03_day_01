be48d808d0b16774379ac4f01555b1871a311382
FALLENau <reece.c.jones@gmail.com>
2017-06-05 11:48:40 +0100

    database starwars

--- jedi.sql
+++ jedi.sql
@@ -5,3 +5,11 @@ CREATE TABLE jedi (
   darkside BOOLEAN,
   age INT2
 );
+
+INSERT INTO jedi (name, darkside, age) VALUES ('Luke', false, 21);
+
+INSERT INTO jedi (name, age) VALUES ('Eugene', 100);
+
+INSERT INTO jedi (name, darkside, age) VALUES ('Vader', true, 101);
+
+SELECT * FROM jedi;
